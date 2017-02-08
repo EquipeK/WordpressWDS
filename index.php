50b21808de4a1e71b9867cc407c84357037b229e
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-02-08 19:31:18 +0100

    Install of wordpress on local

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
  *
  * @package WordPress
  */
-
 /**
  * Tells WordPress to load the WordPress theme and output it.
  *
  * @var bool
  */
 define('WP_USE_THEMES', true);
-
 /** Loads the WordPress Environment and Template */
 require('./wordpress/wp-blog-header.php');
+?>
