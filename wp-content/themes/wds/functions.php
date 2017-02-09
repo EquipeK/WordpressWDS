b1815a534a3fb8e0037c3b5e9e943306bec689b7
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-02-09 03:13:30 +0100

    removing useless additionnal script call

--- wp-content/themes/wds/functions.php
+++ wp-content/themes/wds/functions.php
@@ -107,9 +107,7 @@ add_action( 'widgets_init', 'wds_widgets_init' );
 function wds_scripts() {
 	wp_enqueue_style( 'wds-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'wds-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'wds-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'wds-scripts', get_template_directory_uri() . '/js/scripts.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
