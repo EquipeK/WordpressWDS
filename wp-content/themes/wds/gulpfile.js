78f161c540834b4e556440e5754e6e64b5aa25e9
Occidi <gwendal.jeanson@yahoo.fr>
2017-02-24 11:43:16 +0100

    adding image min to gulpfile

--- wp-content/themes/wds/gulpfile.js
+++ wp-content/themes/wds/gulpfile.js
@@ -4,6 +4,8 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var nano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+
 
 gulp.task('scripts', function () {
     return gulp.src('dev/js/*.js')
@@ -20,10 +22,17 @@ gulp.task('sass', function () {
     .pipe(nano())
     .pipe(gulp.dest('./'));
 });
+ 
+gulp.task('images', function () {
+    gulp.src('dev/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
 
 // Run everything
 gulp.task('watch',function()
 {
     gulp.watch('dev/js/*.js', ['scripts']);
     gulp.watch('dev/sass/**/*.scss', ['sass']);
+    gulp.watch('dev/images/*', ['images']);
 });
