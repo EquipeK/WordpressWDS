45b33a6ab27d419d6a46f9a4722bf05ebe3bd3fe
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-05-10 16:00:48 +0200

    clean un peu du template flex

--- wp-content/themes/wds/front-page.php
+++ wp-content/themes/wds/front-page.php
@@ -12,14 +12,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<div class='header-container'>
-            <div class='header'>
-                <div class='subscribe'>Test &#9662;</div>
-                <div class='logo'><img src="<?php echo(get_template_directory_uri());?>/dist/images/awesome-logo.svg"/></div>
-                <div class='social'><img src='<?php echo(get_template_directory_uri());?>/dist/images/social-icons.svg'/></div>
-            </div>
-        </div>
-
         <div class='photo-grid-container'>
             <div class='photo-grid'>
                 <?php
