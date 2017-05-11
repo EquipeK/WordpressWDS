62b7e40757af735aff5880a3f045c85cfc892b9c
Occidi <gwendal.jeanson@yahoo.fr>
2017-05-11 09:10:52 +0200

    template rubriques

--- wp-content/themes/wds/front-page.php
+++ wp-content/themes/wds/front-page.php
@@ -9,6 +9,14 @@
 
 get_header(); ?>
 
+    <div id="front_formations" style="background-color: #02b77b; height:730px;">
+        
+    </div>
+
+    <div id="front_video" style="height:800px;">
+        
+    </div>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -32,6 +40,14 @@ get_header(); ?>
         </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+    <div id="front_entreprises" style="height:730px;">
+        
+    </div>
+
+    <div id="front_contacts" style="background-color: #02b77b; height:390px;">
+        
+    </div>
 
 <?php
 get_footer();
