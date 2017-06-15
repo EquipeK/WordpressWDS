de97f20e036b5aa6df43073d5fcb1bf7dc6f061d
Occidi <gwendal.jeanson@yahoo.fr>
2017-06-15 10:20:53 +0200

    systeme d'articles

---
+++ wp-content/themes/wds/template-parts/content-article2.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template part for displaying page content in page-exemple.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WDS
+ */
+global $i;
+
+if($i == 1)
+{?>
+	<div id="article-2" style="height:250px; width:100%;">
+    <article style="color:white;" id="actu-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php the_title( '<h6 class="entry-title">', '</h6>' ); ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php
+				the_content();
+			?>
+		</div><!-- .entry-content -->
+
+	</article><!-- #post-## -->
+	</div>
+	<?php 	
+}elseif($i == 2)
+{?>
+    <div id="article-3"  style="height:250px; background-color:#07b57a; width:100%;">
+    <article style="color:white;" id="actu-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php the_title( '<h6 class="entry-title">', '</h6>' ); ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php
+				the_content();
+			?>
+		</div><!-- .entry-content -->
+
+	</article><!-- #post-## -->
+	</div>	
+<?php     
+}?>
