036dccc3393d339da130a3c5af4043554c0d0407
Occidi <gwendal.jeanson@yahoo.fr>
2017-06-22 12:14:37 +0200

    changement design et animation menu

--- wp-content/themes/wds/dev/js/nav-formations.js
+++ wp-content/themes/wds/dev/js/nav-formations.js
@@ -34,14 +34,17 @@ $(document).ready(function() {
 		$('#front_formations').css("background-color", "#efd502");
 	});
 /*Changement d'image*/
-  $("#cf7_controls").on('click', 'span', function() {
+  $("#cf7_controls").on('click', 'li', function() {
+  //$("#cf7_controls").on('click', 'span', function() {
     $("#cf7 img").removeClass("opaque");
 
     var newImage = $(this).index();
 
     $("#cf7 img").eq(newImage).addClass("opaque");
-
-    $("#cf7_controls span").removeClass("selected");
+	$("#cf7_controls li").removeClass("selected");
+	$("#cf7_controls li").removeClass("current");
+    // $("#cf7_controls span").removeClass("selected");
+	$(this).addClass("current");
     $(this).addClass("selected");
   });
 });
