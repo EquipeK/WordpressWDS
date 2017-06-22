4e2ae818ad86899b7a4a95d74b54e01343234379
Christophe GRASSIOT <grassiot.christophe@gmail.com>
2017-06-22 11:21:28 +0200

    WIP Right Column to ContactPage

--- wp-content/themes/wds/page-templates/page-contact.php
+++ wp-content/themes/wds/page-templates/page-contact.php
@@ -29,20 +29,34 @@
                                 <h3>Nos coordonnées</h3>
                             </div>
                             <div class="coordAddress">
-                                <div class="coordIcon"><img src="" alt=""></div>
-                                <div class="coordInfo">
+                                <div class="coordIcon left">
+                                    <img src="<?php echo get_template_directory_uri()?>/dev/images/localisation_white.png" alt="Localisation" class="responsive-img">
+                                    <!-- Icone Black a afficher seulement si la classe est contactCoordRES -->
+                                    <img style="display: none;" src="<?php echo get_template_directory_uri()?>/dev/images/localisation_black.png" alt="Localisation" class="responsive-img">
+                                </div>
+                                <div class="coordInfo right">
                                     <p>297 rue Eugène Berest<br />29200 BREST</p>
                                 </div>
+                                <div class="clearfix"></div>
                             </div>
                             <div class="coordPhone">
-                                <div class="coordIcon"><img src="" alt=""></div>
-                                <div class="coordInfo">
+                                <div class="coordIcon left">
+                                    <img src="<?php echo get_template_directory_uri()?>/dev/images/phone_white.png" alt="Phone" class="responsive-img">
+                                    <!-- Icone Black a afficher seulement si la classe est contactCoordRES -->
+                                    <img style="display: none;" src="<?php echo get_template_directory_uri()?>/dev/images/phone_black.png" alt="Phone" class="responsive-img">
+                                </div>
+                                <div class="coordInfo right">
                                     <p>02 00 00 00 13</p>
                                 </div>
+                                <div class="clearfix"></div>
                             </div>
                             <div class="coordEmail">
-                                <div class="coordIcon"><img src="" alt=""></div>
-                                <div class="coordInfo">
+                                <div class="coordIcon left">
+                                    <img src="<?php echo get_template_directory_uri()?>/dev/images/mail_white.png" alt="Mail" class="responsive-img">
+                                    <!-- Icone Black a afficher seulement si la classe est contactCoordRES -->
+                                    <img style="display: none;" src="<?php echo get_template_directory_uri()?>/dev/images/mail_black.png" alt="Mail" class="responsive-img">
+                                </div>
+                                <div class="coordInfo right">
                                     <p>brest@ecolewds.fr</p>
                                 </div>
                             </div>
