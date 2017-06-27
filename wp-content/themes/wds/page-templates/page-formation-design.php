76bebaee700021298ae6983a18457bb2b1dad35d
Christophe GRASSIOT <grassiot.christophe@gmail.com>
2017-06-27 10:51:30 +0200

    Add Formation Filiere

---
+++ wp-content/themes/wds/page-templates/page-formation-design.php
@@ -0,0 +1,28 @@
+<?php /* Template Name: FormationDesign */ ?>
+
+<?php get_header('custom'); ?>
+    <div style="border:solid 1px black; height:350px; width:100%;margin-top:50px">
+		<div style="margin-top:100px">
+			<span style="padding:25px 30px 25px 30px; border:solid 1px black; border-top-right-radius:50px; border-bottom-right-radius:50px; font-size:1.2em"><?php echo get_the_title(); ?></span>
+		</div>
+	</div>
+
+
+    <section class="whatThis">
+
+    </section>
+
+    <section class="tags">
+
+    </section>
+
+    <section class="moreInfos">
+
+    </section>
+
+    <section class="moreActus">
+
+    </section>
+
+
+<?php get_footer(); ?>
