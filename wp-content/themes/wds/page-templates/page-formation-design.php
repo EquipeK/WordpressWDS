65f5df99825026cf0ac60adede1aa065f2b05b59
Christophe GRASSIOT <grassiot.christophe@gmail.com>
2017-06-29 10:42:58 +0200

    WIP Responsive FormationDesign

--- wp-content/themes/wds/page-templates/page-formation-design.php
+++ wp-content/themes/wds/page-templates/page-formation-design.php
@@ -17,20 +17,20 @@
                 </div>
                 <div class="col m9 whatThisContent">
                     <p>
-                        <span>Le Design Graphique</span> consiste à créer un outil de communication visuelle grâce à différents moyens.
+                        <span class="formationDesignTxt">Le Design Graphique</span> consiste à créer un outil de communication visuelle grâce à différents moyens.
                         C’est une discipline qui s’applique à de nombreux domaines : illustration, presse, édition, packaging, publicité, design web, motion design, communication d’entreprise, identité visuelle, etc.
                     </p>
                     <p>
-                        <span>Le designer graphique</span> doit par conséquent savoir faire preuve d’adaptation et d’une grande créativité.
+                        <span class="formationDesignTxt">Le designer graphique</span> doit par conséquent savoir faire preuve d’adaptation et d’une grande créativité.
                     </p>
                 </div>
-                <a href="" class="btnDownload"><span class="btnText">Télécharger la plaquette</span> <img src="<?php echo get_template_directory_uri()?>/dev/images/iconeDownload.png" alt=""></a>
+                <a href="" class="btnDownload formationDesignTxt"><span class="btnText">Télécharger la plaquette</span> <img src="<?php echo get_template_directory_uri()?>/dev/images/iconeDownload.png" alt=""></a>
             </div>
         </div>
 
     </section>
 
-    <section class="formationTags">
+    <section class="formationTags formationDesignBG">
         <div id="cf7" class="shadow">
             <div class="row">
                 <ul id="cf7_controls">
@@ -40,7 +40,7 @@
                 </ul>
             </div>
             <div class="row" style="right:15%">
-                <div id="content_objectif" class="txt col s9 push-s2">
+                <div id="content_objectif" class="txt col m9 push-m2 s12">
                     <h3 class="titre">Les objectifs</h2>
                     <ul class="obj">
                         <li><div class="objCheck"><img src="<?php echo get_template_directory_uri()?>/dev/images/iconeCheck.png" alt=""></div> Maîtriser les bases de la PAO (Publication Assistée par Ordinateur) et de la création graphique multimédia.</li>
@@ -51,11 +51,11 @@
                         <li><div class="objCheck"><img src="<?php echo get_template_directory_uri()?>/dev/images/iconeCheck.png" alt=""></div> Savoir rédiger et tenir une discussion en anglais.</li>
                     </ul>
                 </div>
-                <div id="content_matiere" class="txt col s9 push-s2">
+                <div id="content_matiere" class="txt col m9 push-m2 s12">
                     <h3 class="titre">Les Matières Enseignées </h2>
                     <img src="<?php echo get_template_directory_uri()?>/dev/images/matieres_design.png" alt="Design Matières" class="responsive-img">
                 </div>
-                <div id="content_debouche" class="txt col s9 push-s2">
+                <div id="content_debouche" class="txt col m9 push-m2 s12">
                     <h3 class="titre">Les Débouchés</h2>
                     <img src="<?php echo get_template_directory_uri()?>/dev/images/tags_design.png" alt="Design Débouchés" class="responsive-img">
                 </div>
@@ -69,19 +69,19 @@
         <div class="row">
             <div class="container">
                 <div class="col s12 m5 push-m7" id="bloc-alternance">
-                    <h6>EN SAVOIR PLUS SUR L'ALTERNANCE</h6>
+                    <h6 class="formationDesignTxt">EN SAVOIR PLUS SUR L'ALTERNANCE</h6>
                     <p>Retrouver les informations sur l'alternance sur notre page Alternance</p>
-                    <a class="waves-effect waves-light btn" id="btn-alternance">Alternance</a>
+                    <a class="waves-effect waves-light btn formationDesignBG" id="btn-alternance">Alternance</a>
                 </div>
             </div>
-            <img src="<?php echo get_template_directory_uri()?>/dev/images/DG2.png" style="position:absolute; left:0; z-index:-1;" alt="">
+            <img src="<?php echo get_template_directory_uri()?>/dev/images/DG2.png" style="position:absolute; left:0; z-index:-1;" alt="" class="hide-on-small-only">
         </div>
     </section>
 
     <section class="moreActus">
         <div id="formation_entreprises" style="display:block;">
             <div class="row">
-                <div id="purple-block" class="col s12 m4 push-m6">
+                <div id="purple-block" class="col s12 m4 push-m6 formationDesignBG">
                     <div id="purple-block-text">
                         <h4>PORTES OUVERTES</h3>
                         <p>Tout les mercredis après-midi, dès le <br> mois de février, l'école ouvre ses portes <br> et vous accueille de 14h à 17h.</p>
