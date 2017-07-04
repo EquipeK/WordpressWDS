c89e437326c7619ff4154f205a4ec3625c2e29a3
Occidi <gwendal.jeanson@yahoo.fr>
2017-07-04 17:19:23 +0200

    new youtube video

--- wp-content/themes/wds/dev/js/youtube.js
+++ wp-content/themes/wds/dev/js/youtube.js
@@ -3,7 +3,7 @@ $(function()
     $('#launch_video').click(function (){
         // Load the IFrame Player API code asynchronously.
         $('#ytapiplayer2').show();
-        $('#ytapiplayer2').append('<iframe width="1130" height="636" src="http://www.youtube.com/embed/NN_zCyMv-Pk?autoplay=0&rel=0" frameborder="0" allowfullscreen></iframe>');
+        $('#ytapiplayer2').append('<iframe width="1130" height="636" src="http://www.youtube.com/embed/w5l1hCw83sA?autoplay=0&rel=0" frameborder="0" allowfullscreen></iframe>');
         $('#bloc_video_pres').hide();
     });
 });
